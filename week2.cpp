26205eb31e9a015121bc29ddeb53656e7cf6bf49
Ayub Mohamed <ayubmohamed_@hotmail.com>
2020-09-23 16:48:37 -0400

    Finished destructor

--- week2.cpp
+++ week2.cpp
@@ -1,26 +1,34 @@
 #include<iostream>
 using namespace std;
 
+
 class Vehicle {
 	int numWheels, numDoors;
 public:
 	Vehicle();
 	Vehicle(int);
 	Vehicle(int, int);
+
+	Vehicle::Vehicle(int w, int d) {
+		cout << "In constructor with 2 parameters" << endl;
+		numWheels = w;
+		numDoors = d;
+	}
+
+	Vehicle::Vehicle(int w) {
+		cout << "In constructor with 1 parameters, wheels = " << w << endl;
+		Vehicle(4, w);
+	}
+
+	Vehicle::Vehicle() {
+		cout << "In constructor with 0 parameters" << endl;
+		Vehicle(4);
+	}
+
+	~Vehicle() {
+		cout << "In destructor" << endl;
+	}
 };
-
-Vehicle::Vehicle(int w, int d) {
-	numWheels = w;
-	numDoors = d;
-}
-
-Vehicle::Vehicle(int w) {
-	Vehicle(4, w);
-}
-
-Vehicle::Vehicle() {
-	Vehicle(4);
-}
 
 int main(int argc, char** argv)
 {
