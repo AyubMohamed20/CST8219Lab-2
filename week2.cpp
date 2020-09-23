85170cdccfdd98fd6a998d679ce6560146a39d54
Ayub Mohamed <ayubmohamed_@hotmail.com>
2020-09-23 17:17:26 -0400

    Demo #1

--- week2.cpp
+++ week2.cpp
@@ -1,26 +1,21 @@
 #include<iostream>
 using namespace std;
 
-
 class Vehicle {
 	int numWheels, numDoors;
 public:
-	Vehicle();
-	Vehicle(int);
-	Vehicle(int, int);
-
-	Vehicle::Vehicle(int w, int d) {
+	Vehicle(int w, int d) {
 		cout << "In constructor with 2 parameters" << endl;
 		numWheels = w;
 		numDoors = d;
 	}
 
-	Vehicle::Vehicle(int w) {
+	Vehicle(int w) {
 		cout << "In constructor with 1 parameters, wheels = " << w << endl;
 		Vehicle(4, w);
 	}
 
-	Vehicle::Vehicle() {
+	Vehicle(void) {
 		cout << "In constructor with 0 parameters" << endl;
 		Vehicle(4);
 	}
@@ -32,6 +27,7 @@ public:
 
 int main(int argc, char** argv)
 {
-	cout << "Hello World!" << endl;
+	Vehicle  myVehicle;
+	cout << "I made a Vehicle!" << endl;
 	return 0;
 }
